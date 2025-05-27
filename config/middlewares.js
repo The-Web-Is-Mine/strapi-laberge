a0008836b113649b1e30697884a7f47cf206c41c
root <davidpigeon@hotmail.com>
2025-05-27 09:23:21 -0400

    Fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,35 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapiclouding.s3.us-east-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapiclouding.s3.us-east-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +37,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]
