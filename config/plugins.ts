16e05f81d04f6b4be06542eed717c1e8bc849205
root <davidpigeon@hotmail.com>
2025-06-03 13:54:45 -0400

    WIP: AWS setup

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,27 +1,47 @@
 export default ({ env }) => ({
-    email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: env('SMTP_HOST', 'smtp.example.com'),
-          port: env('SMTP_PORT', 465),
-          auth: {
-            user: env('SMTP_USERNAME'),
-            pass: env('SMTP_PASSWORD'),
-          },
-          tls: {
-            minVersion: 'TLSv1.2',
-          },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 465),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
         },
-        settings: {
-          defaultFrom: 'sk@growthlabm.com',
-          defaultReplyTo: 'hello@example.com',
+        tls: {
+          minVersion: 'TLSv1.2',
         },
       },
-    },
-    'strapi-v5-plugin-populate-deep': {
-      config: {
-        defaultDepth: 5,
+      settings: {
+        defaultFrom: 'sk@growthlabm.com',
+        defaultReplyTo: 'hello@example.com',
       },
     },
-  })
+  },
+  'strapi-v5-plugin-populate-deep': {
+    config: {
+      defaultDepth: 5,
+    },
+  },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          ACL: env('AWS_ACL', 'public-read'),
+          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+})
